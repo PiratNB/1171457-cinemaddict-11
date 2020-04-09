856e9eeca8005364c405ad14943897e791f68fe8
Alexey <89641234561@mail.ru>
2020-04-09 14:58:56 +0800

    Генерирует базу фильмов на основе моковых данных

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const FILM_COUNT = 5;
+const FILMS_COUNT = 30;
+const FILMS_TO_RENDER = 5;
 const TOP_RATED_COUNT = 2;
 const MOST_COMMENTED_COUNT = 2;
 
@@ -6,51 +7,52 @@ import {createProfileUserTemplate} from "./components/profile-user.js";
 import {createNavigationTemplate} from "./components/navigation.js";
 import {createSortTemplate} from "./components/sort.js";
 import {createFilmsListTemplate} from "./components/film-list.js";
-import {createFilmCardTemplate} from "./components/film-card.js";
+import {createFilmCard} from "./components/film-card.js";
 import {createBtnShowMoreTemplate} from "./components/btn-show-more.js";
 import {createTopRatedMostCommentedTemplate} from "./components/rate-commented.js";
 import {createFooterStatisticsTemplate} from "./components/footer-statistics.js";
 import {createFilmDetailsTemplate} from "./components/film-details.js";
-
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
+import {renderElement} from "./utils";
+import {generateFilmBase} from "./mocks/film-cards";
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = document.querySelector(`.header`);
 
-render(siteHeaderElement, createProfileUserTemplate(), `beforeend`);
-render(siteMainElement, createNavigationTemplate(), `beforeend`);
-render(siteMainElement, createSortTemplate(), `beforeend`);
-render(siteMainElement, createFilmsListTemplate(), `beforeend`);
+renderElement(siteHeaderElement, createProfileUserTemplate()); // Рендер имени пользователя
+renderElement(siteMainElement, createNavigationTemplate()); // Рендер главного меню
+renderElement(siteMainElement, createSortTemplate()); // Рендер панели сортировки
+renderElement(siteMainElement, createFilmsListTemplate()); // Рендер секции с фильмами
+
+// Рендер всех фильмов
+const films = generateFilmBase(FILMS_COUNT);
 
 const filmListContainerElement = siteMainElement.querySelector(`.films-list__container`);
 
-for (let i = 0; i < FILM_COUNT; i++) {
-  render(filmListContainerElement, createFilmCardTemplate(), `beforeend`);
-}
+films.slice(0, FILMS_TO_RENDER).forEach((it) => {
+  renderElement(filmListContainerElement, createFilmCard(it)); // Рендер карточек фильмов
+});
 
 const filmListElement = siteMainElement.querySelector(`.films-list`);
 
-render(filmListElement, createBtnShowMoreTemplate(), `beforeend`);
+renderElement(filmListElement, createBtnShowMoreTemplate()); // Рендер кнопки Loadmore
 
 const filmsElement = siteMainElement.querySelector(`.films`);
 
-render(filmsElement, createTopRatedMostCommentedTemplate(), `beforeend`);
+renderElement(filmsElement, createTopRatedMostCommentedTemplate()); // Рендер секции с самыми популярными и комментируемыми фильмами
 
 const topRatedElement = filmsElement.querySelector(`.top-rated`);
 
 for (let i = 0; i < TOP_RATED_COUNT; i++) {
-  render(topRatedElement, createFilmCardTemplate(), `beforeend`);
+  renderElement(topRatedElement, createFilmCard(films[0])); // Рендео самых популярных фильмов
 }
 
 const mostCommentedElement = filmsElement.querySelector(`.most-commented`);
 
 for (let i = 0; i < MOST_COMMENTED_COUNT; i++) {
-  render(mostCommentedElement, createFilmCardTemplate(), `beforeend`);
+  renderElement(mostCommentedElement, createFilmCard(films[0])); // Рендер самых комментируемых фильмов
 }
 
 const siteFooter = document.querySelector(`.footer`);
 const siteFooterStat = siteFooter.querySelector(`.footer__statistics`);
-render(siteFooterStat, createFooterStatisticsTemplate(), `beforeend`);
-render(siteFooter, createFilmDetailsTemplate(), `afterend`);
+renderElement(siteFooterStat, createFooterStatisticsTemplate()); // Рендер статистики
+// renderElement(siteFooter, createFilmDetailsTemplate(), `afterend`); // Рендер карточки фильма попап
