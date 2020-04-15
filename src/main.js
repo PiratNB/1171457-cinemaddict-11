954ee5660c1b7058293fb2eb2b6a0faaf604dd89
Alexey <89641234561@mail.ru>
2020-04-15 20:19:50 +0800

    Переписывает main, реализует открытие\закрытие попапа

--- src/main.js
+++ src/main.js
@@ -1,71 +1,87 @@
 const FILMS_COUNT = Math.round(Math.random() * 500);
 const FILMS_TO_RENDER = 5;
-const TOP_RATED_COUNT = 2;
-const MOST_COMMENTED_COUNT = 2;
+const FILMS_EXTRA_TO_RENDER = 2;
 
 import ProfileUser from "./components/profile-user";
 import Navigation from "./components/navigation";
 import Sort from "./components/sort";
 import FilmList from "./components/film-list";
-import FilmCard from "./components/film-card";
-import BtnShowMore from "./components/btn-show-more";
 import RateCommented from "./components/rate-commented";
+import FilmCard from "./components/film-card";
+import FilmDetails from "./components/film-details";
+import BtnShowMore from "./components/btn-show-more";
 import FilmStat from "./components/film-stat";
-// import FilmDetails from "./components/film-details";
 import {renderElement} from "./utils";
 import {generateFilmBase} from "./mocks/film-cards";
 
+const renderFilm = (container, film) => {
+  const filmCard = new FilmCard(film);
+  const onFilmPosterClick = (evt) => {
+    if (!(evt.target.className === `film-card__title`) &&
+      !(evt.target.className === `film-card__poster`) &&
+      !(evt.target.className === `film-card__comments`)) {
+      return;
+    }
+    const filmPopup = new FilmDetails(film);
+    const onPopupCloseClick = () => {
+      filmPopup.getElement().remove();
+      filmPopup.removeElement();
+    };
+
+    filmPopup.getElement().querySelector(`button.film-details__close-btn`).addEventListener(`click`, onPopupCloseClick);
+
+    renderElement(document.querySelector(`body`), filmPopup.getElement());
+  };
+
+  filmCard.getElement().addEventListener(`click`, onFilmPosterClick);
+
+  renderElement(container, filmCard.getElement()); // Рендер карточек фильмов
+};
+
 const renderFilmsPack = (container, filmsPack) => {
-  filmsPack.forEach((it) => {
-    renderElement(container, createFilmCard(it)); // Рендер карточек фильмов
+  filmsPack.forEach((film) => {
+    renderFilm(container, film);
   });
 };
 
 const films = generateFilmBase(FILMS_COUNT);
 let filmCardsRendered = Math.min(films.length, FILMS_TO_RENDER);
 
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = document.querySelector(`.header`);
+renderElement(document.querySelector(`header.header`), new ProfileUser(films.filter((it) => it.isWatched).length).getElement()); // Рендер статуса пользователя
+renderElement(document.querySelector(`.footer__statistics`), new FilmStat(films).getElement()); // Рендер общего кол-ва фильмов
+const mainContainer = document.querySelector(`main.main`);
+renderElement(mainContainer, new Navigation(films).getElement());
+renderElement(mainContainer, new Sort().getElement());
+const filmsSection = new FilmList().getElement();
+renderElement(mainContainer, filmsSection);
 
-renderElement(siteHeaderElement, createProfileUser(films.filter((it) => it.isWatched).length)); // Рендер аватарки и статуса пользователя
-renderElement(siteMainElement, createNavigation(films)); // Рендер главного меню
-renderElement(siteMainElement, createSort()); // Рендер панели сортировки
-renderElement(siteMainElement, createFilmsList()); // Рендер секции с фильмами
-const filmsSection = siteMainElement.querySelector(`.films`);
+// Рендер всех фильмов
+const allFilmsList = new RateCommented().getElement();
+renderElement(filmsSection, allFilmsList);
+const allFilmsListContainer = allFilmsList.lastElementChild;
+renderFilmsPack(allFilmsListContainer, films.slice(0, FILMS_TO_RENDER));
 
-const filmListContainerElement = siteMainElement.querySelector(`.films-list__container`);
-
-renderFilmsPack(filmListContainerElement, films.slice(0, FILMS_TO_RENDER));
+const loadmoreButton = new BtnShowMore().getElement();
 if (films.length > filmCardsRendered) {
-  renderElement(filmsSection.querySelector(`.films-list`), createBtnShowMore()); // Рендер кнопки Loadmore
-  const loadMoreButton = filmsSection.querySelector(`.films-list__show-more`);
-  loadMoreButton.addEventListener(`click`, () => {
-    renderFilmsPack(filmListContainerElement, films.slice(filmCardsRendered, filmCardsRendered + FILMS_TO_RENDER));
+  renderElement(allFilmsList, loadmoreButton); // Рендер кнопки Loadmore
+  loadmoreButton.addEventListener(`click`, () => {
+    renderFilmsPack(allFilmsListContainer, films.slice(filmCardsRendered, filmCardsRendered + FILMS_TO_RENDER));
     filmCardsRendered = (filmCardsRendered + FILMS_TO_RENDER) > films.length ? films.length : (filmCardsRendered + FILMS_TO_RENDER);
     if (filmCardsRendered === films.length) {
-      loadMoreButton.classList.add(`visually-hidden`);
+      loadmoreButton.classList.add(`visually-hidden`);
     }
   });
 }
 
-renderElement(filmsSection, createTopRatedMostCommented(), `afterend`); // Рендер секции с самыми популярными и комментируемыми фильмами
-
-// Рендер Top rated и Моst commented фильмов
-renderElement(filmsSection, createTopRatedMostCommented(`Top rated`));
-renderElement(filmsSection, createTopRatedMostCommented(`Most commented`));
-
-const FilmsListContainer = filmsSection.querySelectorAll(`.films-list--extra .films-list__container`);
-
+// Рендер Extra фильмов
+const topRatedFilmsList = new RateCommented(`Top rated`).getElement();
+renderElement(filmsSection, topRatedFilmsList);
 const filmsSortedByRating = films.slice().sort((a, b) => b.rating - a.rating);
+const topRatedFilmsListContainer = topRatedFilmsList.lastElementChild;
+renderFilmsPack(topRatedFilmsListContainer, filmsSortedByRating.slice(0, FILMS_EXTRA_TO_RENDER));
+
+const mostCommentedFilmsList = new RateCommented(`Most commented`).getElement();
+renderElement(filmsSection, mostCommentedFilmsList);
 const filmsSortedByCommentsCount = films.slice().sort((a, b) => b.comments.length - a.comments.length);
-
-const topRatedFilmsListContainer = FilmsListContainer[0];
-renderFilmsPack(topRatedFilmsListContainer, filmsSortedByRating.slice(0, TOP_RATED_COUNT));
-
-const mostCommentedFilmsListContainer = FilmsListContainer[1];
-renderFilmsPack(mostCommentedFilmsListContainer, filmsSortedByCommentsCount.slice(0, MOST_COMMENTED_COUNT));
-
-const siteFooterStat = document.querySelector(`.footer__statistics`);
-renderElement(siteFooterStat, createStat(films)); // Рендер статистики
-
-// renderElement(document.querySelector(`body`), createFilmDetails(films[0]));
+const mostCommentedFilmsListContainer = mostCommentedFilmsList.lastElementChild;
+renderFilmsPack(mostCommentedFilmsListContainer, filmsSortedByCommentsCount.slice(0, FILMS_EXTRA_TO_RENDER));
