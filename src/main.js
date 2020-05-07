6bea96710eff1bd637d1ad97fc7e2d37b3e4b5d6
Alexey <89641234561@mail.ru>
2020-05-07 15:44:48 +0800

    Ревлизует переключение экрана на статистику

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {renderElement} from "./utils/render";
 import {generateMovieBase} from "./mocks/film-cards";
 import PageController from "./controllers/page-controller";
 import MoviesModel from "./models/movies";
+import Statistics from "./components/statistics";
 
 const films = generateMovieBase(FILMS_COUNT);
 
@@ -15,3 +16,20 @@ const moviesModel = new MoviesModel();
 moviesModel.setMovies(films);
 const pageController = new PageController(mainContainer, moviesModel);
 pageController.render();
+
+const statistics = new Statistics();
+renderElement(mainContainer, statistics);
+statistics.hide();
+
+mainContainer.addEventListener(`click`, (evt) => {
+  switch (evt.target.classList[0]) {
+    case `main-navigation__additional`:
+      pageController.hide();
+      statistics.show();
+      break;
+    case `main-navigation__item`:
+      pageController.show();
+      statistics.hide();
+      break;
+  }
+});
