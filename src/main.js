11bb40741d2b4cfbd81409aa4a015afe2e7bf1dd
Alexey <89641234561@mail.ru>
2020-05-26 18:26:16 +0800

    Реализует синхронизацию

--- src/main.js
+++ src/main.js
@@ -56,3 +56,13 @@ apiWithProvider.getFilms()
 window.addEventListener(`load`, () => {
   navigator.serviceWorker.register(`/sw.js`);
 });
+
+window.addEventListener(`online`, () => {
+  document.title = document.title.replace(` [offline]`, ``);
+
+  apiWithProvider.sync();
+});
+
+window.addEventListener(`offline`, () => {
+  document.title += ` [offline]`;
+});
